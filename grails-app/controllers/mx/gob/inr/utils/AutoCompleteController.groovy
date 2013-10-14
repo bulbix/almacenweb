9316fa923aeeb70bfd47e03744c729e12dfb7dce
bulbix <lfpradof@gmail.com>
2013-10-14 13:35:39 -0500

    Permite exponer el api ya construido en almacenes al modulo en enfermeria por medio de jsonp

--- grails-app/controllers/mx/gob/inr/utils/AutoCompleteController.groovy
+++ grails-app/controllers/mx/gob/inr/utils/AutoCompleteController.groovy
@@ -2,12 +2,21 @@ package mx.gob.inr.utils
 
 import mx.gob.inr.farmacia.SalidaFarmacia;
 import grails.converters.JSON
+import grails.plugin.jsonp.JSONP;
 
 class AutoCompleteController {
 
 	AutoCompleteService autoCompleteService	
 	
 	def listarPaciente(){
-		render autoCompleteService.listarPaciente(params.term) as JSON
+				
+		def pacientes = autoCompleteService.listarPaciente(params.term)
+		
+		if (params.callback) {
+			render "${params.callback}(${pacientes as JSON})"
+		} else {
+			render pacientes as JSON
+		}
+		
 	}	
 }
